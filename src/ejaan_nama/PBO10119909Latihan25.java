84b11d6d18a597faebc2f7e5c8eec14508690e65
Anggawan Ridho <anggawanridho@gmail.com>
2020-10-30 23:55:49 +0700

    revise ""

--- src/ejaan_nama/PBO10119909Latihan25.java
+++ src/ejaan_nama/PBO10119909Latihan25.java
@@ -17,7 +17,7 @@ public class PBO10119909Latihan25 {
         System.out.print("Masukkan nama depan anda untuk di eja : ");
         String nama = scanner.next();
         System.out.println();
-        System.out.printf("Ejaan untuk %s adalah : %n", nama);
+        System.out.printf("Ejaan untuk \"%s\" adalah : %n", nama);
         for (int i = 0; i < nama.length(); i++) {
             System.out.printf("Huruf ke-%d : %c%n", i+1, nama.charAt(i));
         }
